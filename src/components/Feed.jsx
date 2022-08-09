c7eeca80742dca01c02af58765c7c42a596c3e67
RaoHarkesh <raoharkesh1998@gmail.com>
2022-08-09 20:01:59 +0530

    storage implemented

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,18 +2,76 @@ import React, { useState } from "react"
 import Videos from "./videos"
 import { signOut } from "firebase/auth"
 import { auth } from "../Firebase"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from "../Firebase"
 export default function Feed() {
-  
+
   return (
     <>
-       <div className="feed-header">
-          <span className="feed-logo">Instagram</span>
-          <img src="https://c8.alamy.com/comp/2HYEGN1/zayn-malik-at-the-2018-pre-grammy-gala-and-salute-to-industry-icons-held-at-the-sheraton-new-york-times-square-hotel-on-january-27-2018-2HYEGN1.jpg" alt="" />
-           <button onClick={()=>{signOut(auth)}}>signout</button>
-       </div>
-       <div className="video-card">
-        <Videos/>
-       </div>
+      <div className="feed-header">
+        <span className="feed-logo">Instagram</span>
+        <img src="https://c8.alamy.com/comp/2HYEGN1/zayn-malik-at-the-2018-pre-grammy-gala-and-salute-to-industry-icons-held-at-the-sheraton-new-york-times-square-hotel-on-january-27-2018-2HYEGN1.jpg" alt="" />
+        <button onClick={() => { signOut(auth) }}>signout</button>
+      </div>
+      <div className="upload">
+        <button>upload</button>
+        <input type="file"
+          onChange={(e) => {
+            let videoobj = e.target.files[0];
+            let { name, type, size } = videoobj;
+            type = type.split('/')[0];
+            if (type !== "video") {
+              alert("please upload a video")
+            }
+            else {
+              // code from firebase
+              // dependencies ref, uploadBytesResumable, getDownloadURL from firebase/storage
+              const storageRef = ref(storage, name);
+
+              const uploadTask = uploadBytesResumable(storageRef, videoobj);
+
+              // Register three observers:
+              // 1. 'state_changed' observer, called any time the state changes
+              // 2. Error observer, called on failure
+              // 3. Completion observer, called on successful completion
+              uploadTask.on('state_changed',
+                (snapshot) => {
+                  // Observe state change events such as progress, pause, and resume
+                  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                  const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  console.log('Upload is ' + progress + '% done');
+                  switch (snapshot.state) {
+                    case 'paused':
+                      console.log('Upload is paused');
+                      break;
+                    case 'running':
+                      console.log('Upload is running');
+                      break;
+                  }
+                },
+                (error) => {
+                  // Handle unsuccessful uploads
+                },
+                () => {
+                  // Handle successful uploads on complete
+                  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                  });
+                }
+              );
+
+            }
+
+          }}
+        />
+      </div>
+      <div className="reels-container">
+        <div className="reels-middle-box">
+          <Videos />
+        </div>
+
+      </div>
     </>
   )
 }
--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -3,7 +3,9 @@ const Videos=()=>{
    const [playing,setPlaying] = useState(false)
    return(
     <>
-    <video onClick={(e)=>{
+    <div className="single-reel">
+      <div className="upper-section">
+      <video className="video" onClick={(e)=>{
       if(playing){
         e.currentTarget.pause();
         setPlaying(false)
@@ -11,9 +13,53 @@ const Videos=()=>{
         e.currentTarget.play();
         setPlaying(true)
       }
-    }} src="">
-
+    }} src="https://firebasestorage.googleapis.com/v0/b/class-demo-94ab5.appspot.com/o/I%20am%20a%20rider%20%23fitmanjeet%20%23shorts%20%23fitmanjeetshorts%20%23bodybuilder%20%23trendring.mp4?alt=media&token=7324c572-22f5-4ffa-812b-7db2641b995e">
+     
     </video>
+      </div>
+      <div className="bottom-section">
+        <span className="reel-title">Song name</span>
+        <input type="text" placeholder="comments" />    
+      </div>
+    </div>
+    <div className="single-reel">
+      <div className="upper-section">
+      <video className="video" onClick={(e)=>{
+      if(playing){
+        e.currentTarget.pause();
+        setPlaying(false)
+      }else{
+        e.currentTarget.play();
+        setPlaying(true)
+      }
+    }} src="https://firebasestorage.googleapis.com/v0/b/class-demo-94ab5.appspot.com/o/I%20am%20a%20rider%20%23fitmanjeet%20%23shorts%20%23fitmanjeetshorts%20%23bodybuilder%20%23trendring.mp4?alt=media&token=7324c572-22f5-4ffa-812b-7db2641b995e">
+     
+    </video>
+      </div>
+      <div className="bottom-section">
+        <span className="reel-title">Song name</span>
+        <input type="text" placeholder="comments" />    
+      </div>
+    </div>
+    <div className="single-reel">
+      <div className="upper-section">
+      <video className="video" onClick={(e)=>{
+      if(playing){
+        e.currentTarget.pause();
+        setPlaying(false)
+      }else{
+        e.currentTarget.play();
+        setPlaying(true)
+      }
+    }} src="https://firebasestorage.googleapis.com/v0/b/class-demo-94ab5.appspot.com/o/I%20am%20a%20rider%20%23fitmanjeet%20%23shorts%20%23fitmanjeetshorts%20%23bodybuilder%20%23trendring.mp4?alt=media&token=7324c572-22f5-4ffa-812b-7db2641b995e">
+     
+    </video>
+      </div>
+      <div className="bottom-section">
+        <span className="reel-title">Song name</span>
+        <input type="text" placeholder="comments" />    
+      </div>
+    </div>
     </>
    )    
 }
